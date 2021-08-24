3ba0e0718d612339dd22aed520b402372b198674
abhi5884 <70086450+ab745@users.noreply.github.com>
2021-08-24 14:33:36 +0530

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -16,7 +16,11 @@ const help: string =
 
 export const Help = Composer.command('help', async ctx => {
     
-    if (ctx.chat.type === 'channel', 'group', 'supergroup') return await ctx.reply("This Command works in private Only");
+    if (ctx.chat.type === 'channel') return await ctx.reply("This Command works in private Only");
+    
+    if (ctx.chat.type === 'supergroup') return await ctx.reply("This Command works in private Only");
+    
+    if (ctx.chat.type === 'group') return await ctx.reply("This Command works in private Only");
     
     if (ctx.chat.type === 'private') return await ctx.replyWithMarkdownV2(help);
     
