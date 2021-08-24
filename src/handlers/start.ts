ad75c92d4c22bbfa1210fda0044b5c01ffd83ff9
abhi5884 <70086450+ab745@users.noreply.github.com>
2021-08-24 14:03:14 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -12,8 +12,10 @@ import { escape } from 'html-escaper';
 export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${escape(ctx.from.first_name + " " + ctx.from.last_name)}</a>\n` +
-        `I Play Songs in Group Voice Chats.\n` +
-        `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`
+        `I'm <b>Lycia</b>, <i>The Music Bot</i>.\n` +
+        `I can Play Songs in Group Voice Chats.\n` +
+        `\n` +
+        `Maintained by <a href="https://t.me/NeuroticAssociation">Neurotic Association</a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
             [Markup.button.url('View Source', 'https://github.com/ArnabXD/TGVCBot')]
