233210c37cec1e97a01c0c63f102559d64c920ba
abhi5884 <70086450+ab745@users.noreply.github.com>
2021-08-24 14:05:17 +0530

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -4,11 +4,11 @@ import { Composer } from 'telegraf';
 const help: string =
     `*Commands Available :*\n\n` +
     `/jiosaavn or /jsvn : Play songs from JioSaavn\n` +
-    `/youtube or /yt : Play songs from YouTube\n` +
+    `/ytplay or /yt : Play songs from YouTube\n` +
     `/play : Reply this command to audio files to play the file\n` +
-    `/queue or /q : Check queued songs list\n` +
-    `/p : Pause the stream\n` +
-    `/r : Resume the stream\n` +
+    `/queue or /next : Check queued songs list\n` +
+    `/p or /pause : Pause the stream\n` +
+    `/r or /pause: Resume the stream\n` +
     `/next or /skip : Skip the current song\n` +
     `/stopvc : Stop the stream\n` +
     `/help : Show this Menu`
