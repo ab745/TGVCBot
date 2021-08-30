ecead83ee5437eb1e4d72b733f78ae8319ba7b96
abhi5884 <70086450+ab745@users.noreply.github.com>
2021-08-30 16:01:55 +0530

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -6,13 +6,12 @@ export default composer;
 
 const help =
  `*Commands Available :*\n\n` +
-    `\n` +  
     `/jiosaavn or /jsvn : Play songs from JioSaavn\n` +
     `/ytplay or /yt : Play songs from YouTube\n` +
     `/play : Reply this command to audio files to play the file\n` +
     `/queue or /playlist : Check queued songs list\n` +
     `/p or /pause : Pause the stream\n` +
-    `/r or /pause: Resume the stream\n` +
+    `/r or /resume: Resume the stream\n` +
     `/next or /skip : Skip the current song\n` +
     `/stopvc : Stop the stream\n` +
     `/help : Show this Menu`;
