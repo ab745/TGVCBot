192d229d4564f251b77d71225d326deef9d33685
abhi5884 <70086450+ab745@users.noreply.github.com>
2021-08-30 15:41:25 +0530

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -5,15 +5,16 @@ const composer = new Composer();
 export default composer;
 
 const help =
-  `*Commands Available :*\n\n` +
-  `/jiosaavn or /jsvn : Play songs from JioSaavn\n` +
-  `/youtube or /yt : Play songs from YouTube\n` +
-  `/play : Reply this command to audio files to play the file\n` +
-  `/queue or /q : Check queued songs list\n` +
-  `/p : Pause the stream\n` +
-  `/r : Resume the stream\n` +
-  `/next or /skip : Skip the current song\n` +
-  `/stopvc : Stop the stream\n` +
-  `/help : Show this Menu`;
+ `*Commands Available :*\n\n` +
+    `\n` +  
+    `/jiosaavn or /jsvn : Play songs from JioSaavn\n` +
+    `/ytplay or /yt : Play songs from YouTube\n` +
+    `/play : Reply this command to audio files to play the file\n` +
+    `/queue or /playlist : Check queued songs list\n` +
+    `/p or /pause : Pause the stream\n` +
+    `/r or /pause: Resume the stream\n` +
+    `/next or /skip : Skip the current song\n` +
+    `/stopvc : Stop the stream\n` +
+    `/help : Show this Menu`;
 
 composer.command('help', ctx => ctx.reply(help, { parse_mode: 'MarkdownV2' }));
